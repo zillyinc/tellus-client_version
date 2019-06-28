56c2c9a1eae526e0de0be515866833cab1f8a410
Calvin Tuong <calvin.tuong@gmail.com>
2019-06-28 12:51:19 -0700

    Initial commit

---
+++ lib/tellus/client_version.rb
@@ -0,0 +1,8 @@
+require "tellus/client_version/version"
+
+module Tellus
+  module ClientVersion
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/tellus/client_version/version.rb
@@ -0,0 +1,5 @@
+module Tellus
+  module ClientVersion
+    VERSION = "0.1.0"
+  end
+end
